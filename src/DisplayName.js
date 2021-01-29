fa6ec610bca874d48e81bdfe3c2c2ed48bce2e38
PratimKumarHazarika <PratimKumarHazarika@users.noreply.github.com>
2021-01-29 13:22:16 +0530

    update : functionalComponents

---
+++ src/DisplayName.js
@@ -0,0 +1,11 @@
+export function DisplayName(props){
+    return <h1>My name is Pratim</h1>
+};
+
+export  const DisplayAge= (props) =>{
+    return <h1>My age is 21</h1>
+};
+
+export function CityName(props) {
+    return <h1>I am from Mumbai</h1>  
+};
